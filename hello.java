a77375e3e694cb00c4f19215c2ebb50ae53fa919
Shiwani <shiwanichahar32@gmail.com>
2025-06-24 11:41:23 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    public static void main(String []args){
+        //comment
+        /*multiline comment */
+        System.out.println("Hello World !");
+        System.out.println("I am learning JAVA. ");
+        System.out.println("This is my first java program ");
+    }
+}
+
---
+++ variable.java
@@ -0,0 +1,19 @@
+public class variable {
+    public static void main(String[] args) {
+        int age = 22;
+        System.out.println(age);
+    }
+    /*
+     * data types 
+     * int 
+     * float
+     * double 
+     * boolean 
+     * char
+     * string
+     * byte 
+     * short
+     * long
+     
+     */
+}
